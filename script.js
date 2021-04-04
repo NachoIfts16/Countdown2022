29f2abf868f2f1649b21dcc57e3ebd17122e9f12
NachoIfts16 <elalba98@hotmail.com>
2021-04-04 16:02:55 -0300

    final code

---
+++ script.js
@@ -0,0 +1,33 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = '1 Jan 2022';
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    //1h = 3600segs
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+    
+}
+//Cuando el numero del countdown sea menor a 10 se ponga un '0' delante
+//Ej: 02 hours 05mins 09segs.
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+countdown();
+
+
+setInterval(countdown, 1000);
